933d014a785810eabdc26197e0b61129aeb0336c
maxchehab <maxchehab@gmail.com>
2017-11-15 21:32:04 -0800

    Created write lock file and copy changes method

--- index.js
+++ index.js
@@ -3,8 +3,14 @@
 const chalk = require("chalk");
 const finder = require("find-root");
 const jsonfile = require("jsonfile");
+const fs = require("fs-extra");
 
-const { dependencyBuilder, compare } = require("./lib/utils");
+const {
+  dependencyBuilder,
+  compare,
+  copyChanges,
+  writeLockFile
+} = require("./lib/utils");
 const log = console.log;
 
 try {
@@ -12,14 +18,26 @@ try {
   const name = jsonfile.readFileSync(root + "/package.json").name;
 
   dependencyBuilder(name, root);
-  compare(name, root);
+
+  let changes = compare(name, root);
+  if (changes.length) {
+    console.log("> Detected " + changes.length + " changes:");
+  } else {
+    console.log("> No changes detected.");
+  }
+  changes.forEach(function(entry) {
+    console.log("\t" + chalk.red(entry.relativePath));
+  });
+
+  copyChanges(changes, root);
+
+  writeLockFile(changes, root);
 } catch (err) {
   if (err.message == "package.json not found in path") {
     log(
       chalk.red(`Couldn't find any dependencies. Try creating a package.json`)
     );
   } else {
-    //log(chalk.red(err.message));
     throw err;
   }
 }
