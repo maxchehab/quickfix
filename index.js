ac88ee57887783043cafb1e60bed2b787127703c
maxchehab <maxchehab@gmail.com>
2017-11-15 19:33:57 -0800

    Implemented diff, yarn check, and cache installation

--- index.js
+++ index.js
@@ -2,24 +2,24 @@
 
 const chalk = require("chalk");
 const finder = require("find-root");
-const path = require("path");
-const { dependencyBuilder, downloadDependency } = require("./lib/utils");
+const jsonfile = require("jsonfile");
+
+const { dependencyBuilder, compare } = require("./lib/utils");
 const log = console.log;
 
 try {
   const root = finder(process.cwd()).toString();
+  const name = jsonfile.readFileSync(root + "/package.json").name;
 
-  dependencies = dependencyBuilder(root);
-
-  for (dependency of dependencies) {
-    downloadDependency(dependency);
-  }
+  dependencyBuilder(name, root);
+  compare(name, root);
 } catch (err) {
   if (err.message == "package.json not found in path") {
     log(
       chalk.red(`Couldn't find any dependencies. Try creating a package.json`)
     );
   } else {
-    log(chalk.red(err.message));
+    //log(chalk.red(err.message));
+    throw err;
   }
 }
